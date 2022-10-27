e026956a12908f9a56ba21f708f24ce533480dd4
Nagico <yjr888@vip.qq.com>
2022-10-27 13:51:00 +0800

    fix: 更新模型

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
     MainWindow window(800, 600);
 
     window.loadModel("assets/model/bunny_texture/bunny.obj");
-//    window.loadModel("assets/model/bunny/bunny.obj");
+    //window.loadModel("assets/model/bun_zipper.ply");
 
     window.exec();
     return 0;
